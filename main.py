2e78d3d6f40016a77343a4710d5de312fbb45e30
FrdrcDpn <frederic.dupon@hotmail.com>
2017-03-16 10:57:34 +0100

    cool things I changed

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    print("hello world")
-=======
 
     print('this is project')
->>>>>>> 83af2e3e5082f7a7a07bc6de34fd580786c48f6a
+    print("epic bike shizzle")
+    print("analyzing shit n stuff")
