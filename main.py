855d7ddd495e86a5a83f2ed07a45c23b81edaa67
spiera <sybe.piera@hotmail.com>
2017-03-16 12:30:20 +0100

    Did some stupid shit

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-"""this is the main."""
+def func():
+    """Stupid func.
+
+    Args:
+        nothing = nothing
+
+    Returns:
+        1 (int) = 1.
+    """
+    return 1
 
 if __name__ == "__main__":
     print('do you agree Lex sucks?)
@@ -7,3 +16,5 @@ if __name__ == "__main__":
 
     if a == 'yes':
         print("hello world")
+
+    func()
