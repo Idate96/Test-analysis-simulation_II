9760679865b4e127dde0515dc58c5e5b007e7682
kdekievit <krijndekievit@ziggo.nl>
2017-03-16 12:30:37 +0100

    added function

--- test3.py
+++ test3.py
@@ -1,13 +1,14 @@
-print('hello')
 
-import pdb
-xtab = []
-ytab = []
-for i in range(10):
-    x = i*i #square
-    y = (i-1)*i**2
-    xtab.append(x)
-    ytab.append(y)
 
-print(xtab)
-print(ytab)
+def func(v):
+    xtab = []
+    ytab = []
+    for i in range(v):
+        x = i*i #square
+        y = (i-1)*i**2
+        xtab.append(x)
+        ytab.append(y)
+
+    return xtab+ytab
+
+print(func(10))
