ed7ad0b6bad92def46af58a5b052225335b63111
FrdrcDpn <frederic.dupon@hotmail.com>
2017-03-16 12:13:24 +0100

    fixed grammar

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 """this is the main."""
 
 if __name__ == "__main__":
-    print('do you agree Lex sucks?)
-    print (1)
+    print('do you agree Lex sucks?')
+    print(1)
     a = input()
 
     if a == 'yes':
