a02effd4bd4b78aee8851226fa79a78f35eaf2ab
tchstolk <toonstolk@gmail.com>
2017-03-17 12:10:52 +0100

    the function to make a testlist is now complete

--- toon_frederic_epic_file.py
+++ toon_frederic_epic_file.py
@@ -3,22 +3,26 @@
 print("here is where the magic will happen")
 TestList = [[(0.2, 0.2, 0, 0, 0, 0), (0.4, 0.2, 0, 1, 0, 0), (0.6, 0.2, 0, 2, 0, 0), (0.8, 0.2, 0, 1, 0, 0), (1.0, 0.2, 0, 0, 0, 0)], [(0.2, 0.4, 0, 1, 0, 0), (0.4, 0.4, 0, 2, 0, 0), (0.6, 0.4, 0, 3, 0, 0), (0.8, 0.4, 0, 2, 0, 0), (1.0, 0.4, 0, 1, 0, 0)], [(0.2, 0.6, 0, 2, 0, 0), (0.4, 0.6, 0, 3, 0, 0), (0.6, 0.6, 0, 4, 0, 0), (0.8, 0.6, 0, 3, 0, 0), (1.0, 0.6, 0, 2, 0, 0)], [(0.2, 0.6, 0, 2, 0, 0), (), (), (), ()], [(), (), (), (), ()]]
 
-def Make_TestList():
-    """
-    This function makes a testlist that consists of 5 lists, each with 5 tuples
-    to represent the data received by 1.
 
+def Make_TestList():
+    """This function makes a testlist.
+     that consists of 5 lists, each with 5.
+    tuples to represent the data received by 1.
     takes in nothing, starts at the origin with steps of 0.2 till 1
     """
-
     def tuple_returner(i, j):
         return (i * 0.2, j * 0.2, 0, i+j, 0, 0)
     ans = []
     semi_ans = []
-    for i in range(6):
-        for j in range(6):
+    for j in range(6):
+        for i in range(6):
+            semi_ans.append(tuple_returner(i, j))
+        ans.append(semi_ans)
+
+    return ans
 
 
+print(Make_TestList())
 
 
 print("Here we gooo!!!")
