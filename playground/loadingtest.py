f7f8b8ef6cb256f22e1a37fcfa3e65efc413d312
aertstutvoet <aert.stutvoet@gmail.com>
2017-03-17 12:07:29 +0100

    loading definition

--- playground/loadingtest.py
+++ playground/loadingtest.py
@@ -1,7 +1,4 @@
 import numpy as np
 
-test = np.array([0,0]:[1,2,3],
-        [1,0]:[2,3,4],
-        [1,1]:[3,4,5])
-
-print(grades[0,0])
+data = np.loadtxt('exp_data_kopie\StD_vel04.plt')
+print(data[1])
