806d81883fcd32c9819336a1e090fdc7211e552e
aertstutvoet <aert.stutvoet@gmail.com>
2017-03-17 12:21:23 +0100

    changes to loading

--- src/loading.py
+++ src/loading.py
@@ -3,17 +3,24 @@
 import numpy as np
 
 #loading the data (StD_vel04)
-
-location = 'exp_data_kopie\StD_vel04.plt'
 def load(x,y,location):
-    data = np.loadtxt(loc)
-    numb_loc = y*64 + x
+    if location==1:
+        location = 'exp_data_kopie\StD_vel04(1).plt'
+    if location==2:
+        location = 'exp_data_kopie\StD_vel16(2).plt'
+    if location==3:
+        location = 'exp_data_kopie\Velocity_04(3).plt'
+    if location==4:
+        location = 'exp_data_kopie\Velocity_16(4).plt'
+    else:
+        print "for location fill in the corresponding value in brackets in exp_data_kopie"
+    data = np.loadtxt(location)
+    numb_loc = y*53 + x
     return data[numb_loc]
 
-print(load(0,0,loc))
 
-
-#matrix[0]=[1,1,1,1,1,1]
-#print(matrix)
-#data = np.loadtxt('exp_data_kopie\StD_vel04.plt')
-#print(data)
+print(load(0,0,1))
+#example how to use it:
+#location = 'exp_data_kopie\StD_vel04.plt'
+#print(load(0,0,location))
+#this will print the first row
