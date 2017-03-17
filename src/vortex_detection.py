5826303b7c5a51cbc96055fd83c6817bdd97597a
kdekievit <krijndekievit@ziggo.nl>
2017-03-17 16:43:33 +0100

    Changed some small things in vortex identification

--- src/vortex_detection.py
+++ src/vortex_detection.py
@@ -19,17 +19,21 @@ def discrete_method(velgrid):
         pos_pt (Tuple) = storage number of a point in the grid , ie (1,0).
         velgrid (np array) = velocity components in the grid.
     """
+
+    #The indices of the vortex centers will be stored in a list.
+
     vortex_center_ind = []
     for i, row in enumerate(velgrid):
         for j, element in enumerate(velgrid):
-            vel0 = velgrid[i,j]
-            vel1 = velgrid[i-1,j]
-            vel2 = velgrid[i,j+1]
-            vel3 = velgrid[i+1,j]
-            vel4 = velgrid[i,j-1]
+            vel0 = velgrid[i,j]     #velocity of the actual point
+            vel1 = velgrid[i-1,j]   #velocity of the left point
+            vel2 = velgrid[i,j+1]   #velocity of the top point
+            vel3 = velgrid[i+1,j]   #velocity of the right point
+            vel4 = velgrid[i,j-1]   #velocity of the bottom point
 
             check1 = np.sign(vel1[0])+np.sign(vel3[0])+np.sign(vel2[1])+np.sign(vel4[1])
             check2 = np.sign(vel1[0])+np.sign(vel2[1])
-            check3 = np.sign(vel1[0])
-            if int(check1) == 0 and int(check2) != 0 and int(check3) != 0:
+            check3 =
+
+            if (int(check1) == 0) and (int(check2) != 0) and not (np.all(vel1) == 0):
                 vortex_center_ind.append((i,j))
