7406315ba8183b512a142ae4a25302868f76c22d
Lorenzo <lorenzoterenzi96@gmail.com>
2017-03-17 09:19:44 +0100

    Cleaned the shit around

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-def func():
-    """Stupid func.
-
-    Args:
-        nothing = nothing
-
-    Returns:
-        1 (int) = 1.
-    """
-    return 1
 
 if __name__ == "__main__":
-
-    print('do you agree Toon sucks?')
-
-    a = input()
+    
