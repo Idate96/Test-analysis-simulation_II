63a73d43f6e8c12631452932225ea093ed6e2399
kdekievit <krijndekievit@ziggo.nl>
2017-03-17 12:18:49 +0100

    I have added a module to define vortex centers, it will contain multiple conditions to define vortices

---
+++ src/vortex_detection.py
@@ -0,0 +1,35 @@
+"""This module contains different condition algorithms.
+
+to detect vortices in the velocity field.
+"""
+
+import numpy as np
+
+def discrete_method(velgrid):
+    """This module uses a discrete method to determine vortex centers.
+
+    First it evaluates at every point if the condition for a vortex center
+
+    holds. Then it lets these vortices grow until no new points that satisfy
+
+    the condition are found.
+
+    Arg:
+
+        pos_pt (Tuple) = storage number of a point in the grid , ie (1,0).
+        velgrid (np array) = velocity components in the grid.
+    """
+    vortex_center_ind = []
+    for i, row in enumerate(velgrid):
+        for j, element in enumerate(velgrid):
+            vel0 = velgrid[i,j]
+            vel1 = velgrid[i-1,j]
+            vel2 = velgrid[i,j+1]
+            vel3 = velgrid[i+1,j]
+            vel4 = velgrid[i,j-1]
+
+            check1 = np.sign(vel1[0])+np.sign(vel3[0])+np.sign(vel2[1])+np.sign(vel4[1])
+            check2 = np.sign(vel1[0])+np.sign(vel2[1])
+            check3 = np.sign(vel1[0])
+            if int(check1) == 0 and int(check2) != 0 and int(check3) != 0:
+                vortex_center_ind.append((i,j))
