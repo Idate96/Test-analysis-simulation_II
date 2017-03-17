f6c1863fa78ffbefda4198a0e4042e41e40daa54
aertstutvoet <aert.stutvoet@gmail.com>
2017-03-17 12:27:13 +0100

    loading definition

--- src/loading.py
+++ src/loading.py
@@ -4,6 +4,9 @@ import numpy as np
 
 #loading the data (StD_vel04)
 def load(x,y,location):
+    #for location fill in the corresponding value in brackets in exp_data_kopie
+    #0<=x<=64
+    #0<=x<=64
     if location==1:
         location = 'exp_data_kopie\StD_vel04(1).plt'
     if location==2:
@@ -12,8 +15,6 @@ def load(x,y,location):
         location = 'exp_data_kopie\Velocity_04(3).plt'
     if location==4:
         location = 'exp_data_kopie\Velocity_16(4).plt'
-    else:
-        print "for location fill in the corresponding value in brackets in exp_data_kopie"
     data = np.loadtxt(location)
     numb_loc = y*53 + x
     return data[numb_loc]
@@ -21,5 +22,5 @@ def load(x,y,location):
 
 
 #example how to use it:
-#print(load(0,0,1))
+print(load(0,0,1))
 #this will print the first row
