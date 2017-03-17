e2f2c63672a0cb234febf231ad8f0cd87fe804d5
tchstolk <toonstolk@gmail.com>
2017-03-17 11:10:54 +0100

    okay now it is okay

--- toon_frederic_epic_file.py
+++ toon_frederic_epic_file.py
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
-"""this is task number 2."""
+"""this is task number 2"""
+
 
 print("here is where the magic will happen")
-=======
-"""this is task number 2"""
+
 
 print("Here we gooo!!!")
 # we should know what data structure we receive
@@ -14,4 +13,3 @@ print("Here we gooo!!!")
 # then we can make a clear conclusion between 4 and 16 m/s about the viscosity
 # This can be done using a planar graph of the velocity and let them intersect
 # eachother
->>>>>>> c49fe553958fde65fcd08568ca58e98b41168e80
