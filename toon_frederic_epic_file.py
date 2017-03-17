39bffd391564fa07356644b6ed92d34f07245d28
tchstolk <toonstolk@gmail.com>
2017-03-17 11:43:38 +0100

    added a function to create a testlist

--- toon_frederic_epic_file.py
+++ toon_frederic_epic_file.py
@@ -1,7 +1,18 @@
 """this is task number 2."""
 
 print("here is where the magic will happen")
-TestList = [[(0.2, 0.2, 0, 0, 0,0), (0.4, 0.2, 0, 1, 0, 0), (0.6, 0.2, 0, 2, 0, 0), (0.8, 0.2, 0, 1, 0, 0), (1.0, 0.2, 0, 0, 0, 0)], [(0.2, 0.4, 0, 1, 0, 0), (0.4, 0.4, 0, 2, 0, 0), (0.6, 0.4, 0, 3, 0, 0), (0.8, 0.4, 0, 2, 0, 0), (1.0, 0.4, 0, 1, 0, 0)], [(0.2, 0.6, 0, 2, 0, 0), (0.4, 0.6, 0, 3, 0, 0), (), (), ()], [(), (), (), (), ()], [(), (), (), (), ()]]
+TestList = [[(0.2, 0.2, 0, 0, 0, 0), (0.4, 0.2, 0, 1, 0, 0), (0.6, 0.2, 0, 2, 0, 0), (0.8, 0.2, 0, 1, 0, 0), (1.0, 0.2, 0, 0, 0, 0)], [(0.2, 0.4, 0, 1, 0, 0), (0.4, 0.4, 0, 2, 0, 0), (0.6, 0.4, 0, 3, 0, 0), (0.8, 0.4, 0, 2, 0, 0), (1.0, 0.4, 0, 1, 0, 0)], [(0.2, 0.6, 0, 2, 0, 0), (0.4, 0.6, 0, 3, 0, 0), (0.6, 0.6, 0, 4, 0, 0), (0.8, 0.6, 0, 3, 0, 0), (1.0, 0.6, 0, 2, 0, 0)], [(0.2, 0.6, 0, 2, 0, 0), (), (), (), ()], [(), (), (), (), ()]]
+
+def Make_TestList(x, y):
+    """
+    This function makes a testlist that consists of 5 lists, each with 5 tuples
+    to represent the data received by 1.
+
+    takes in a starting x and y value
+    """
+    ans = []
+    semi_ans = []
+    semi_ans_ans = ()
 
 print("Here we gooo!!!")
 # we should know what data structure we receive
@@ -12,4 +23,3 @@ print("Here we gooo!!!")
 # then we can make a clear conclusion between 4 and 16 m/s about the viscosity
 # This can be done using a planar graph of the velocity and let them intersect
 # eachother
-print(TestList[0])
