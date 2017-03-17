20e81586566e07a06692bf3e796473e9f2905b45
tchstolk <toonstolk@gmail.com>
2017-03-17 10:58:40 +0100

    added description of task 2 in comment line

--- toon_frederic_epic_file.py
+++ toon_frederic_epic_file.py
@@ -1,1 +1,11 @@
 """this is task number 2"""
+
+print("Here we gooo!!!")
+# we should know what data structure we receive
+# with that we can work to get the components of the velocity at different
+# points
+# the main task is to get the freestream component of the flow and divide it by
+# the freestream velocity
+# then we can make a clear conclusion between 4 and 16 m/s about the viscosity
+# This can be done using a planar graph of the velocity and let them intersect
+# eachother
