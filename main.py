4d2d0a29a220500cc5e72a31e177565258fb141d
Lorenzo <lorenzoterenzi96@gmail.com>
2017-03-17 08:44:53 +0100

    cleaned

--- main.py
+++ main.py
@@ -14,13 +14,3 @@ if __name__ == "__main__":
     print('do you agree Toon sucks?)
 
     a = input()
-
-<<<<<<< HEAD
-    if a == 'yes':
-        print("hello world")
-
-    func()
-=======
-    if a == 'no':
-        print("HACKERMAN was here")
->>>>>>> fdcf0118ea5ea76f73f989bfab9292dddafbb656
