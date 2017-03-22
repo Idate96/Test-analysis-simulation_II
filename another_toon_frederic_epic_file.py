f3c915c8aa845b9820fe8bd3dce1029f94b7b025
FrdrcDpn <frederic.dupon@hotmail.com>
2017-03-22 14:26:27 +0100

    updates in another epic file

--- another_toon_frederic_epic_file.py
+++ another_toon_frederic_epic_file.py
@@ -1,3 +1,8 @@
+"""this file contains the xy plot of the velocity field and stuff."""
+
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 import numpy as np
+from load_file import *
+import vortex_detection
+import vorticity_strain
