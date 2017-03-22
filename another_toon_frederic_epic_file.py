33e48e6a2277716b80b56fa60228d35023992404
FrdrcDpn <frederic.dupon@hotmail.com>
2017-03-22 14:30:22 +0100

    made changes toon

--- another_toon_frederic_epic_file.py
+++ another_toon_frederic_epic_file.py
@@ -6,3 +6,9 @@ import numpy as np
 from load_file import *
 import vortex_detection
 import vorticity_strain
+
+if __name__ == '__another_toon_frederic_epic_file__':
+    exp = '04'
+    x, y, vel = load_data(exp)
+    xx, yy = np.meshgrid(x, y)
+    
