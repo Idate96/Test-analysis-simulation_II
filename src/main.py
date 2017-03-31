a140a3c63a6980f329ac46a23981795dab8d04ab
kdekievit <krijndekievit@ziggo.nl>
2017-03-31 09:21:07 +0200

    made sure graphs arent saved

--- src/main.py
+++ src/main.py
@@ -23,63 +23,43 @@ if __name__ == '__main__':
     # plot_data_3d(xx, yy, vel[2, :, :])
     #
     # # countour plotting
-    # countour_data_plot(xx, yy, vel[0, :, :], 'Normalized x velocity', 'u/U', save=False)
-    # countour_data_plot(xx, yy, vel[1, :, :], 'Normalized y velocity', 'v/U', save=False)
-    # countour_data_plot(xx, yy, vel[2, :, :], 'Normalized z velocity', 'w/U', save=False)
+    countour_data_plot(xx, yy, vel[0, :, :], 'Normalized x velocity', 'u/U', save=False)
+    countour_data_plot(xx, yy, vel[1, :, :], 'Normalized y velocity', 'v/U', save=False)
+    countour_data_plot(xx, yy, vel[2, :, :], 'Normalized z velocity', 'w/U', save=False)
     #
 
-    #quiver_data_plot(xx, yy, vel[2, :, :], vel[:2, :, :],
-<<<<<<< HEAD
-                     #'Normalized z velocity', 'w/U', save=True)
-=======
-    #                 'Normalized z velocity', 'w/U', save=True)
->>>>>>> 79415eed91d37ac9b2db4587047d686544e3ada0
+    quiver_data_plot(xx, yy, vel[2, :, :], vel[:2, :, :],
+                     'Normalized z velocity', 'w/U', save=False)
 
     # vortex detection preliminaries
     u_grad, v_grad, w_grad = vorticity_strain.velocity_gradients(vel)
     u_grad, v_grad, w_grad = np.asarray(u_grad), np.asarray(v_grad), np.asarray(w_grad)
-    # quiver_data_plot(xx, yy, vel[0, :, :], u_grad,
-    #                  'u', 'u/U', save=False)
+    quiver_data_plot(xx, yy, vel[0, :, :], u_grad,
+                      'u', 'u/U', save=False)
     vorticity_value = vorticity_strain.find_vorticity(u_grad, v_grad)
     strain = vorticity_strain.find_strain(u_grad, v_grad)
     vort_tens = vorticity_strain.construct_vorticity_tensor(u_grad, v_grad)
     strain_tensor = vorticity_strain.contruct_strain_tensor(u_grad, v_grad)
 
     # Quiver with vorticity overlap
-    #quiver_data_plot(xx, yy, vorticity_value, vel[:2, :, :], 'Vorticity', 'w/U', save=False)
+    quiver_data_plot(xx, yy, vorticity_value, vel[:2, :, :], 'Vorticity', 'w/U', save=False)
 
     # plot strain and vorticity
-    #countour_data_plot(xx, yy, strain, 'Strain', r'S', save=False)
-    #countour_data_plot(xx, yy, vorticity_value, 'Vorticity', 'Omega [1\s]', save=False)
-    #vortexcenter_scatter_plot(xx, yy, vorticity_value, vortex_detection.discrete_method(
-        #vel), 'Vorticity', 'Omega [1\s]', save=False)
+    countour_data_plot(xx, yy, strain, 'Strain', r'S', save=False)
+    countour_data_plot(xx, yy, vorticity_value, 'Vorticity', 'Omega [1\s]', save=False)
+    vortexcenter_scatter_plot(xx, yy, vorticity_value, vortex_detection.discrete_method(
+        vel), 'Vorticity', 'Omega [1\s]', save=False)
     quiver_data_plot(xx, yy, vel[2, :, :], vel[:2, :, :],
                      'Normalized z velocity', 'w/U', vortex_centers=vortex_detection.discrete_method(vel),
-                     save=True, show=True)
-
-    quiver_data_plot(xx, yy, vorticity_value, vel[:2, :, :],
-                     'Vorticity', 'Omega [1\s]', vortex_centers=vortex_detection.discrete_method(vel),
-                     save=True, show=False)
-    
-    # Add mannequin: put show=False in plot you want to see the mannequin in
-    AddMannequin()
-
+                     save=False)
 
     # vortex detection
     q = vortex_detection.q_test(vort_tens, strain_tensor)
     mask = np.where(q > 0)
     q[mask] *= 10
-<<<<<<< HEAD
     #countour_data_plot(xx, yy, q, 'Q test vortex detection', 'Q', save=False)
-=======
-    countour_data_plot(xx, yy, q, 'Q test vortex detection', 'Q', save=True)
->>>>>>> 79415eed91d37ac9b2db4587047d686544e3ada0
 
     delta = vortex_detection.delta_test(u_grad, v_grad)
     mask = np.where(delta > 0)
     delta[mask] *= 10
-<<<<<<< HEAD
     #countour_data_plot(xx, yy, delta, 'Delta vortex detection', 'Delta', save=False)
-=======
-    countour_data_plot(xx, yy, delta, 'Delta vortex detection', 'Delta', save=True)
->>>>>>> 79415eed91d37ac9b2db4587047d686544e3ada0
