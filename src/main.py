776ecd122feb2d519ddcd57db4f324378dc8d676
spiera <sybe.piera@hotmail.com>
2017-03-31 09:51:40 +0200

    .

--- src/main.py
+++ src/main.py
@@ -28,14 +28,14 @@ if __name__ == '__main__':
     countour_data_plot(xx, yy, vel[2, :, :], 'Normalized z velocity', 'w/U', save=False)
     #
 
-<<<<<<< HEAD
+
     quiver_data_plot(xx, yy, vel[2, :, :], vel[:2, :, :],
                      'Normalized z velocity', 'w/U', save=False)
-=======
+
     #quiver_data_plot(xx, yy, vel[2, :, :], vel[:2, :, :],
                      #'Normalized z velocity', 'w/U', save=True)
 
->>>>>>> d4a6ccf3e00d562c0b5a1ed1d2eb983535903e60
+
 
     # vortex detection preliminaries
     u_grad, v_grad, w_grad = vorticity_strain.velocity_gradients(vel)
@@ -57,8 +57,7 @@ if __name__ == '__main__':
         vel), 'Vorticity', 'Omega [1\s]', save=False)
     quiver_data_plot(xx, yy, vel[2, :, :], vel[:2, :, :],
                      'Normalized z velocity', 'w/U', vortex_centers=vortex_detection.discrete_method(vel),
-<<<<<<< HEAD
-                     save=True, show=False)
+                      save=True, show=False)
 
     # Add mannequin: put show=False in plot you want to see the mannequin in
     AddMannequin()
@@ -70,32 +69,27 @@ if __name__ == '__main__':
     # Add mannequin: put show=False in plot you want to see the mannequin in
     AddMannequin()
 
-=======
-                     save=False)
->>>>>>> 21951ab7c455c578dad7af542439b040a2a7d5b2
+
+
 
     # vortex detection
     q = vortex_detection.q_test(vort_tens, strain_tensor)
     mask = np.where(q > 0)
     q[mask] *= 10
-<<<<<<< HEAD
+
     #countour_data_plot(xx, yy, q, 'Q test vortex detection', 'Q', save=False)
-=======
+
     countour_data_plot(xx, yy, q, 'Q test vortex detection', 'Q', save=True, show=False)
 
     AddMannequin()
->>>>>>> d4a6ccf3e00d562c0b5a1ed1d2eb983535903e60
+
 
     delta = vortex_detection.delta_test(u_grad, v_grad)
     mask = np.where(delta > 0)
     delta[mask] *= 10
-<<<<<<< HEAD
+
     #countour_data_plot(xx, yy, delta, 'Delta vortex detection', 'Delta', save=False)
-=======
+
     countour_data_plot(xx, yy, delta, 'Delta vortex detection', 'Delta', save=True, show=False)
 
     AddMannequin()
-<<<<<<< HEAD
-=======
->>>>>>> d4a6ccf3e00d562c0b5a1ed1d2eb983535903e60
->>>>>>> 21951ab7c455c578dad7af542439b040a2a7d5b2
